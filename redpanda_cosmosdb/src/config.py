e332dbefee75be5d18539539bbe5a3e64ba1fc6d
Artem Oppermann <artem.oppermann@uni-oldenburg.de>
2024-03-27 08:50:04 +0100

    Create config.py

---
+++ redpanda_cosmosdb/src/config.py
@@ -0,0 +1,8 @@
+import os
+
+settings = {
+	'host': os.environ.get('ACCOUNT_HOST', 'https://<cosmosinstance-name>.documents.azure.com:443/'),
+	'master_key': os.environ.get('ACCOUNT_KEY', '<cosmosdbprimarykey>'),
+	'database_id': os.environ.get('COSMOS_DATABASE', '<cosmosdb-name>'),
+	'container_id': os.environ.get('COSMOS_CONTAINER', <cosmosdb-container>),
+}
